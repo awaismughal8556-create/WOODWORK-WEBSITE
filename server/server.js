3906fd5d25d3aaa5bad3bd8a9d3328834dda0557
Awais Mughal <YOUR-GITHUB-EMAIL>
2026-09-22 19:37:11 +0500

    Update website files

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // ================= MIDDLEWARE =================
 app.use(cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "https://woodwork-website-production.up.railway.app",
     credentials: true
 }));
 
